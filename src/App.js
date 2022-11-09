2334236643c59940263fed5a0fbdde303f7ea96f
borjadb <borja881@outlook.com>
2022-11-09 13:56:53 +0100

    Print table with elements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter} from 'reactstrap'
+import React, { useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const dataWords = [
+    {id: 1, name: "behind", description: "detrás"},
+    {id: 2, name: "beside", description: "junto a"},
+    {id: 3, name: "above", description: "arriba"},
+    {id: 4, name: "below", description: "abajo"},
+  ]
+
+  const [data, setdata] = useState(dataWords);
+
+  return(
+    <Container>
+      <br />
+      <Button>Insert word</Button>
+      <br />
+      <Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Word</th>
+            <th>Description</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(element=>(
+            <tr>
+              <td>{element.id}</td>
+              <td>{element.name}</td>
+              <td>{element.description}</td>
+              <td><Button>Edit</Button> <Button>Delete</Button></td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+
+  )
 }
 
 export default App;
